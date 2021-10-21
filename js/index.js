157f14cc6d09ae32f9c9cb877770896f3d3c6306
nkmichel <nkmichel@gmail.com>
2021-10-21 11:21:10 -0400

    js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(".research").click(function(){
+    $(".research img").fadeIn();
+    $(".research p").fadeIn("slow");
+    
+    
+  });
+  $(".design").click(function(){
+    $(".design img").fadeIn();
+    $(".design p").fadeIn("slow");
+    
+    
+  });
+  
